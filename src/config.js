4ff8e2efcbd834e9ddfb86a9212f35d06e5fa1ca
Paul Llanque <llanquepol@gmail.com>
2023-06-16 10:50:16 -0500

    add: config variables

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const BASE_URI = 'https://codeable-github-stats-api.herokuapp.com/';
+export const GITHUB_API_URL = 'https://api.github.com/users/';
+export const TOKEN = 'token-github-stats';
