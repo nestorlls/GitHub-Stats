fdc109752f58b76cebbf1d04ed53a3e322fee426
Paul Llanque <llanquepol@gmail.com>
2023-06-15 23:10:11 -0500

    add: RegisterForm component

---
+++ src/components/compositeComponents/RegisterForm.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import Input from '../baseComponents/Input';
+import Button from '../baseComponents/Button';
+import { validate } from '../../Utils/form';
+
+const initialValues = { email: '', password: '', firstName: '', lastName: '' };
+
+const RegisterForm = () => {
+  const [form, setForm] = useState({
+    values: initialValues,
+    errors: {},
+    touched: {},
+  });
+
+  const { values, errors, touched } = form;
+  const isValid = Object.values(validate(values)).every(
+    (value) => value === ''
+  );
+
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    const values = { ...form.values, [name]: value };
+    const errors = validate(values);
+    setForm({ ...form, values, errors });
+  }
+
+  function handleBlur(e) {
+    const name = e.target.name;
+    const errors = validate(values);
+    setForm((prev) => ({
+      ...prev,
+      errors,
+      touched: { ...prev.touched, [name]: true },
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('form', form);
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="inputs">
+        <div>
+          <Input
+            label="Email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            type="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.email && touched.email && (
+            <p className="form-error">{errors.email}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            label="Password"
+            name="password"
+            id="password"
+            placeholder="Enter your email"
+            type="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.password && touched.password && (
+            <p className="form-error">{errors.password}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            label="First Name"
+            name="firstName"
+            id="firstName"
+            placeholder="Enter your first name"
+            type="firstName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.firstName && touched.firstName && (
+            <p className="form-error">{errors.firstName}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            label="Last Name"
+            name="lastName"
+            id="lastName"
+            placeholder="Enter your first name"
+            type="lastName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.lastName && touched.lastName && (
+            <p className="form-error">{errors.lastName}</p>
+          )}
+        </div>
+      </div>
+      <Button
+        field="Register"
+        type="submit"
+        disabled={!isValid && 'disabled'}
+        nameClass="primary"
+      />
+    </form>
+  );
+};
+
+export default RegisterForm;
