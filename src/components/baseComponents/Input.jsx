59b64f31838895d53829f37a6eb60760b3b2a3a7
Paul Llanque <llanquepol@gmail.com>
2023-06-15 22:23:06 -0500

    update: input component

--- src/components/baseComponents/Input.jsx
+++ src/components/baseComponents/Input.jsx
@@ -8,11 +8,12 @@ const Input = ({
   value,
   placeholder,
   onChange,
+  onBlur,
 }) => {
   return (
     <div className="flex flex-col gap-1">
       {label && (
-        <label htmlFor={id || name} className="text-md text-gray-600">
+        <label htmlFor={id || name} className="text-md font-medium text-white">
           {label}
         </label>
       )}
@@ -23,6 +24,7 @@ const Input = ({
         value={value}
         placeholder={placeholder}
         onChange={onChange}
+        onBlur={onBlur}
         className="p-2 border border-gray-300 rounded-md"
       />
     </div>
