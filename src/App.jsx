dead528f3d21b4279840921669d3cce5d6b70234
Paul Llanque <llanquepol@gmail.com>
2023-06-15 19:03:52 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LoginOrRegister from './components/pageComponents/LoginOrRegister';
+
+function App() {
+  return (
+    <>
+      <LoginOrRegister />
+    </>
+  );
+}
+
+export default App;
