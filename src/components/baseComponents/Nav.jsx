24b50dd346d5733cbf10756ca4c8fac5efbaf3ea
Paul Llanque <llanquepol@gmail.com>
2023-06-16 21:13:40 -0500

    add: nav component

---
+++ src/components/baseComponents/Nav.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const Nav = () => {
+  const { pathname } = useLocation();
+  const inActiveLink = 'flex gap-2 items-center p-2';
+  const activeLink = `${inActiveLink} bg-gray-200 drop-shadow-2xl rounded-md text-gray-900`;
+
+  return (
+    <aside>
+      <nav className="text-white flex justify-between p-3">
+        <Link to="/" className={pathname === '/' ? activeLink : inActiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </Link>
+        <Link
+          to="/profile"
+          className={pathname.includes('/profile') ? activeLink : inActiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+        </Link>
+        <Link
+          to="/favorites"
+          className={
+            pathname.includes('/favorites') ? activeLink : inActiveLink
+          }>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            />
+          </svg>
+        </Link>
+      </nav>
+    </aside>
+  );
+};
+
+export default Nav;
