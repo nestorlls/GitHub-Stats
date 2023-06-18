05aa02390028f28be4e217bf111b45743ca95c11
Paul Llanque <llanquepol@gmail.com>
2023-06-17 20:32:00 -0500

    add favorite component

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/pageComponents/Layout';
+
+const Favorites = () => {
+  return (
+    <Layout>
+      <h1>Favorites</h1>
+    </Layout>
+  );
+};
+
+export default Favorites;
