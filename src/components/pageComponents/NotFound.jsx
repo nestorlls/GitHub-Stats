f4d8dc89d9438e14b3c0931aabb63b5a4eb6a866
Paul Llanque <llanquepol@gmail.com>
2023-06-18 15:12:15 -0500

    add animation github icon

--- src/components/pageComponents/NotFound.jsx
+++ src/components/pageComponents/NotFound.jsx
@@ -1,9 +1,11 @@
 import { BsGithub } from 'react-icons/bs';
 
 const NotFound = ({ error, loading }) => {
+  const animate = loading?.includes('Retrieving') ? 'animate-spin' : '';
+
   return (
     <div className="flex flex-col gap-4 w-full items-center text-white">
-      <BsGithub size={100} />
+      <BsGithub size={100} className={animate} />
       <p className="text-2xl">{loading}</p>
       {error && <p className="text-red-500">{error}</p>}
     </div>
