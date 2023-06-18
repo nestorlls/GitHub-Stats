12c66c020860891936fa5d039ca3fc12a2523c5a
Paul Llanque <llanquepol@gmail.com>
2023-06-17 21:13:15 -0500

    update Button component

--- src/components/baseComponents/Button.jsx
+++ src/components/baseComponents/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Button = ({ type = 'button', disabled, field, nameClass }) => {
+const Button = ({ type = 'button', disabled, field, nameClass, onclick }) => {
   return (
     <button
       type={type}
       disabled={disabled}
-      className={`btn-${disabled ? 'disabled' : nameClass}`}>
+      className={`btn-${disabled ? 'disabled' : nameClass}`}
+      onClick={onclick}>
       {field}
     </button>
   );
