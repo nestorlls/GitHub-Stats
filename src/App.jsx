5fae107708d47954f756cd0e7638adbdcafedd13
Paul Llanque <llanquepol@gmail.com>
2023-06-17 23:25:21 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import LoginOrRegister from './components/pageComponents/LoginOrRegister';
+import { useUser } from './context/UserContext';
+import { Toaster, toast } from 'sonner';
+import Root from './routes/Root';
 
 function App() {
+  const { user, error } = useUser();
+
+  if (error) {
+    toast.error(error.message);
+  }
+
+  if (user) {
+    toast.success(`Logged in as ${user.first_name}`);
+    return (
+      <>
+        <Toaster position="top-center" richColors />
+        <Root />
+      </>
+    );
+  }
+
   return (
     <>
+      <Toaster position="top-center" richColors />
       <LoginOrRegister />
     </>
   );
