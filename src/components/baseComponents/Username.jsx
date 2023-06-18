e74de9232e4f56a7e0b710accc3f5a36f2aacee9
Paul Llanque <llanquepol@gmail.com>
2023-06-17 20:33:01 -0500

    add username component

---
+++ src/components/baseComponents/Username.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Username = ({ username }) => {
+  return (
+    <p className="text-2xl font-bold text-center text-white p-1">{username}</p>
+  );
+};
+
+export default Username;
