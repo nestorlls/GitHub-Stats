e6cafbf2eb11e46c7dd08bc0f4fee0f50e8b21a4
Paul Llanque <llanquepol@gmail.com>
2023-06-18 15:38:52 -0500

    update Start component

--- src/components/baseComponents/Start.jsx
+++ src/components/baseComponents/Start.jsx
@@ -1,3 +1,5 @@
+import { Link, useLocation, useParams } from 'react-router-dom';
+
 function convertToK(count) {
   const convertedToString = count.toString();
   const converted =
@@ -7,15 +9,21 @@ function convertToK(count) {
   return converted;
 }
 
-const Start = ({ svg, text, count }) => {
+const Start = ({ svg, text, count, path }) => {
   const transformedCount = convertToK(count);
+  const { username } = useParams();
+  const { pathname } = useLocation();
+
+  const ref = pathname === '/' ? `/${path}` : `/favorites/${username}/${path}`;
 
   return (
-    <div className="w-[140px] h-[140px] bg-white p-4 rounded flex flex-col justify-center items-center">
-      <img src={svg} alt={text} />
-      <span className="text-2xl font-bold">{transformedCount}</span>
-      <p>{text}</p>
-    </div>
+    <Link to={ref}>
+      <div className="w-[140px] h-[140px] bg-white p-4 rounded flex flex-col justify-center items-center">
+        <img src={svg} alt={text} />
+        <span className="text-2xl font-bold">{transformedCount}</span>
+        <p>{text}</p>
+      </div>
+    </Link>
   );
 };
 
