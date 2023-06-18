b3a947f629c6225e0c32652b45b5fc8d7b87bb72
Paul Llanque <llanquepol@gmail.com>
2023-06-17 20:35:01 -0500

    add StartSection component

---
+++ src/components/compositeComponents/StartSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Start from '../baseComponents/Start';
+import followersIcon from '../../assets/icons/followers.svg';
+import followingsIcon from '../../assets/icons/followings.svg';
+import public_repoIcon from '../../assets/icons/public_repo.svg';
+import public_gistsIcon from '../../assets/icons/public_gists.svg';
+
+const StartSection = ({ followers, following, public_repos, public_gists }) => {
+  return (
+    <div className="w-96 md:w-full grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 justify-center justify-items-center gap-4 mx-auto py-6 px-10">
+      <Start svg={followersIcon} text="followers" count={followers} />
+      <Start svg={followingsIcon} text="followings" count={following} />
+      <Start svg={public_repoIcon} text="Public repos" count={public_repos} />
+      <Start svg={public_gistsIcon} text="Public gists" count={public_gists} />
+    </div>
+  );
+};
+
+export default StartSection;
