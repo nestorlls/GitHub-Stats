4e491fd89b5a2fd9bbf66a7fd99117227f2f26ae
Paul Llanque <llanquepol@gmail.com>
2023-06-18 15:37:24 -0500

    update sectionCard adding path

--- src/components/compositeComponents/StartSection.jsx
+++ src/components/compositeComponents/StartSection.jsx
@@ -8,10 +8,30 @@ import public_gistsIcon from '../../assets/icons/public_gists.svg';
 const StartSection = ({ followers, following, public_repos, public_gists }) => {
   return (
     <div className="w-96 md:w-full grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 justify-center justify-items-center gap-4 mx-auto py-6 px-10">
-      <Start svg={followersIcon} text="followers" count={followers} />
-      <Start svg={followingsIcon} text="followings" count={following} />
-      <Start svg={public_repoIcon} text="Public repos" count={public_repos} />
-      <Start svg={public_gistsIcon} text="Public gists" count={public_gists} />
+      <Start
+        svg={followersIcon}
+        text="followers"
+        count={followers}
+        path="followers"
+      />
+      <Start
+        svg={followingsIcon}
+        text="followings"
+        count={following}
+        path="followings"
+      />
+      <Start
+        svg={public_repoIcon}
+        text="Public repos"
+        count={public_repos}
+        path="public_repos"
+      />
+      <Start
+        svg={public_gistsIcon}
+        text="Public gists"
+        count={public_gists}
+        path="public_gists"
+      />
     </div>
   );
 };
