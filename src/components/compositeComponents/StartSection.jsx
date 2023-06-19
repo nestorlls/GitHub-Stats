09e0dde4c48607b2a43b31d7cf5df837e5175a90
Paul Llanque <llanquepol@gmail.com>
2023-06-18 20:10:07 -0500

    update StartSection component

--- src/components/compositeComponents/StartSection.jsx
+++ src/components/compositeComponents/StartSection.jsx
@@ -5,7 +5,13 @@ import followingsIcon from '../../assets/icons/followings.svg';
 import public_repoIcon from '../../assets/icons/public_repo.svg';
 import public_gistsIcon from '../../assets/icons/public_gists.svg';
 
-const StartSection = ({ followers, following, public_repos, public_gists }) => {
+const StartSection = ({
+  followers,
+  following,
+  public_repos,
+  public_gists,
+  user,
+}) => {
   return (
     <div className="w-96 md:w-full grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 justify-center justify-items-center gap-4 mx-auto py-6 px-10">
       <Start
@@ -13,24 +19,28 @@ const StartSection = ({ followers, following, public_repos, public_gists }) => {
         text="followers"
         count={followers}
         path="followers"
+        user={user}
       />
       <Start
         svg={followingsIcon}
         text="followings"
         count={following}
         path="followings"
+        user={user}
       />
       <Start
         svg={public_repoIcon}
         text="Public repos"
         count={public_repos}
         path="public_repos"
+        user={user}
       />
       <Start
         svg={public_gistsIcon}
         text="Public gists"
         count={public_gists}
         path="public_gists"
+        user={user}
       />
     </div>
   );
