473caa052bcfa60dbab93cbd3dadd21ed8e3f72c
Paul Llanque <llanquepol@gmail.com>
2023-06-19 17:59:26 -0500

    update style login page

--- src/components/pageComponents/LoginOrRegister.jsx
+++ src/components/pageComponents/LoginOrRegister.jsx
@@ -14,7 +14,7 @@ const LoginOrRegister = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="flex flex-col w-full gap-2 mt-24">
+        <div className="flex flex-col w-full gap-2 mt-24 mx-2">
           <h1 className="text-4xl font-bold text-white mb-12 text-center">
             Welcome to Github Stats
           </h1>
