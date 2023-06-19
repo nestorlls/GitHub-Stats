2ffe5c8d754dfe73c8c337e5d047c8f5bb27e859
Paul Llanque <llanquepol@gmail.com>
2023-06-19 18:04:38 -0500

    update paddind eje x

--- src/components/pageComponents/Layout.jsx
+++ src/components/pageComponents/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <section className="section">
       <div className="container">
         <div className="flex flex-col flex-grow">
-          <div className="flex flex-1">{children}</div>
+          <div className="flex flex-1 mx-2">{children}</div>
           <Nav />
         </div>
       </div>
