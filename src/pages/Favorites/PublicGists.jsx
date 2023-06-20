639321a3a624c534c4b40bf09e875a425c7edd0a
Paul Llanque <llanquepol@gmail.com>
2023-06-19 23:04:50 -0500

    refactor methods in pucli gists

--- src/pages/Favorites/PublicGists.jsx
+++ src/pages/Favorites/PublicGists.jsx
@@ -1,53 +1,16 @@
-import { useEffect, useState } from 'react';
 import Layout from '../../components/pageComponents/Layout';
 import NotFound from '../../components/pageComponents/NotFound';
 import GoToPage from '../../components/compositeComponents/GoToPage';
 import { getCurrentPage, totalPage } from '../../Utils/goToPage';
 import { useParams } from 'react-router-dom';
-import { apiFecthStarts } from '../../services/gitHubServices';
 import ListGists from '../../components/pageComponents/ListGists';
+import { useCurrentPages, useGetStatsUser } from '../../Utils/cumtomeUtils';
 
 const PublicGists = () => {
   const { username, user } = useParams();
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [publicGists, setPublicGists] = useState({
-    data: [],
-    error: null,
-    loading: 'Not having any public gists',
-  });
-
-  const { data, error, loading } = publicGists;
-
-  useEffect(() => {
-    setPublicGists({
-      data: [],
-      error: null,
-      loading: 'loading public gists...',
-    });
-
-    const getPublicGists = setTimeout(async () => {
-      try {
-        const response = await apiFecthStarts(username ?? user, 'gists');
-        if (response.message) {
-          setPublicGists({ data: [], error: null, loading: response.message });
-        }
-        setPublicGists({ data: response, error: null, loading: null });
-      } catch (error) {
-        setPublicGists({ data: [], error: error, loading: null });
-      }
-    }, 500);
-
-    return () => clearTimeout(getPublicGists);
-  }, [username, user]);
-
-  function goNextPage() {
-    setCurrentPage(currentPage + 1);
-  }
-
-  function goPrevPage() {
-    setCurrentPage(currentPage - 1);
-  }
+  const { data, error, loading } = useGetStatsUser(username, user, 'gists');
+  const { currentPage, goNextPage, goPrevPage } = useCurrentPages();
 
   const size = 4;
   const totalPages = totalPage(size, data);
