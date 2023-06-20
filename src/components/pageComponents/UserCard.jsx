6cc0cb963a2b337db39cf38defcb7076c5ad6286
Paul Llanque <llanquepol@gmail.com>
2023-06-19 22:23:38 -0500

    add UserCard component

---
+++ src/components/pageComponents/UserCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NotFound from './NotFound';
+import GitHubCard from './GitHubCard';
+
+const UserCard = ({ data, error, loading }) => {
+  console.log(data);
+  return (
+    <div className="w-full mt-2">
+      {!data ? (
+        <>
+          <NotFound loading={loading} error={error} />
+        </>
+      ) : (
+        <>
+          <GitHubCard {...data} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default UserCard;
