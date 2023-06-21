aff6064956f172b14159cd91f6f79edba5eac202
Paul Llanque <llanquepol@gmail.com>
2023-06-20 22:31:14 -0500

    add favorite provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { UserProvider } from './context/UserContext.jsx';
+import { FavoriteProvider } from './context/FavoriteContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+      <FavoriteProvider>
+        <App />
+      </FavoriteProvider>
     </UserProvider>
   </React.StrictMode>
 );
