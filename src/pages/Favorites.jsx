8834ccc542abf0ef51c8cbe0c2ff526cc6d366fd
Paul Llanque <llanquepol@gmail.com>
2023-06-20 22:34:11 -0500

    refactor creating favorite context

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,45 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import Layout from '../components/pageComponents/Layout';
 import List from '../components/pageComponents/ListCard';
-import FavoritesData from '../services/favoriteServices';
 import NotFound from '../components/pageComponents/NotFound';
 import { getCurrentPage, totalPage } from '../Utils/goToPage';
 import GoToPage from '../components/compositeComponents/GoToPage';
 import { useCurrentPages } from '../Utils/cumtomeUtils';
+import { useFavorites } from '../context/FavoriteContext';
 
 const Favorites = () => {
-  const [favorites, setFavorites] = useState({
-    data: [],
-    error: null,
-    loading: 'No favorites...',
-  });
+  const { favorites } = useFavorites();
 
   const { currentPage, goNextPage, goPrevPage } = useCurrentPages();
   const { data, error, loading } = favorites;
-
-  useEffect(() => {
-    setFavorites({
-      data: [],
-      error: null,
-      loading: 'Loading favorites...',
-    });
-
-    const getFavoritesFrom = setTimeout(async () => {
-      try {
-        const response = await FavoritesData.getFavorites();
-
-        if (response.message) {
-          setFavorites({ data: [], error: response.message, loading: null });
-          return;
-        }
-
-        setFavorites({ data: response, error: null, loading: null });
-      } catch (error) {
-        setFavorites({ data: [], error: error, loading: null });
-      }
-    }, 500);
-    return () => clearTimeout(getFavoritesFrom);
-  }, []);
 
   const size = 6;
   const totalPages = totalPage(size, data);
